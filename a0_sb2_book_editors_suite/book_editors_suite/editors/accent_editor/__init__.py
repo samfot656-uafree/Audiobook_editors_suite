64354b3bf9077f3d4221e79d15cab5534368541d
samfot656-uafree <samfot656@gmail.com>
2025-11-30 16:56:19 +0200

    Create __init__.py
    Create project folder structure

---
+++ a0_sb2_book_editors_suite/book_editors_suite/editors/accent_editor/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+/storage/emulated/0/a0_sb2_book_editors_suite/book_editors_suite/editors/accent_editor/__init__.py
+
+Пакет редактора наголосів.
+"""
+
+from .main import AccentEditorApp
+
+__all__ = ['AccentEditorApp']
+
+
+
+# book_editors_suite/editors/accent_editor/__init__.py
+#"""
+#Accent Editor - редактор наголосів та фонетичної розмітки
+#"""
+
+#from .main import AccentEditor
+
+#__all__ = ['AccentEditor']
+
+
+# book_editors_suite/editors/accent_editor/__init__.py
+#"""
+#Accent Editor - редактор наголосів та фонетичної розмітки
+#"""
+
+#from .main import AccentEditor
+
+#__all__ = ['AccentEditor']
+
+#"""
+#Accent Editor - редактор наголосів та фонетичної розмітки
+#"""
+
+#from .main import AccentEditor
+
+#__all__ = ['AccentEditor']
