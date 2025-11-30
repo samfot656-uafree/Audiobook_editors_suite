4b75038bb330860d04852196c6e697825c6a5455
samfot656-uafree <samfot656@gmail.com>
2025-11-30 17:06:23 +0200

    Create __init__.py
    Create project folder structure

---
+++ a0_sb2_book_editors_suite/book_editors_suite/editors/multispeaker_tts/__init__.py
@@ -0,0 +1,26 @@
+# book_editors_suite/editors/multispeaker_tts/__init__.py
+"""
+Multispeaker TTS - мультиспікерне синтезування мовлення
+"""
+
+#from .main import MultispeakerTTS
+
+__all__ = ['MultispeakerTTS']
+
+
+# book_editors_suite/editors/multispeaker_tts/__init__.py
+#"""
+#Multispeaker TTS - мультиспікерне синтезування мовлення
+#"""
+
+#from .main import MultispeakerTTS
+
+#__all__ = ['MultispeakerTTS']
+
+#"""
+#Multispeaker TTS - мультиспікерне синтезування мовлення
+#"""
+
+#from .main import MultispeakerTTS
+
+#__all__ = ['MultispeakerTTS']
