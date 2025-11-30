cc40fc6df38141c5aa2a4ce99b88b575578010f1
samfot656-uafree <samfot656@gmail.com>
2025-11-30 16:35:11 +0200

    Create __init__.py
    Create project folder structure

---
+++ a0_sb2_book_editors_suite/book_editors_suite/ui/popups/__init__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+/storage/emulated/0/a0_sb2_book_editors_suite/book_editors_suite/ui/popups/__init__.py
+
+Пакет попапів.
+"""
+
+from .edit_word_popup import EditWordPopup
+from .extra_buttons_popup import ExtraButtonsPopup
+
+__all__ = [
+    'EditWordPopup',
+    'ExtraButtonsPopup'
+]
+
+
+
+# book_editors_suite/ui/popups/__init__.py
+#"""
+#Popups module - діалогові вікна та спливаючі меню
+#"""
+
+#from .edit_word_popup import EditWordPopup
+#from .extra_buttons_popup import ExtraButtonsPopup
+
+#__all__ = [
+#    'EditWordPopup',
+#    'ExtraButtonsPopup'
+#]
+
+
+
+
+# book_editors_suite/ui/popups/__init__.py
+#"""
+#Popups module - діалогові вікна та спливаючі меню
+#"""
+
+#from .edit_word_popup import EditWordPopup
+#from .extra_buttons_popup import ExtraButtonsPopup
+
+#__all__ = [
+#    'EditWordPopup',
+#    'ExtraButtonsPopup'
+#]
+
+#"""
+#Popups module - діалогові вікна та спливаючі меню
+#"""
+
+#from .edit_word_popup import EditWordPopup
+#from .extra_buttons_popup import ExtraButtonsPopup
+
+#__all__ = [
+#    'EditWordPopup',
+#    'ExtraButtonsPopup'
+#]
