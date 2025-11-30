e67f6bb8d3401ff8a330f4120d926eb869f174eb
samfot656-uafree <samfot656@gmail.com>
2025-11-30 17:12:29 +0200

    Create s_1_str_book_project.py
    Create project folder structure

---
+++ a0_sb2_book_editors_suite/migrants_rab/s_1_str_book_project.py
@@ -0,0 +1,40 @@
+
+оновлена структура проекту книги
+
+/storage/emulated/0/book_projects/
+└── book_project_name/
+         ├── json/
+         │      ├── book_project_name_config.json
+         │      ├── accent_files.json         
+         │      ├── sound_effects_list.json
+         │      └── sound_effects_files.json
+         ├── inputs/
+         │      ├── book_text_file/
+         │      │      └── book_project_name.txt
+         │      ├── input_melodu/ 
+         │      │      ├── PAUSE_4_mp3 
+         │      │      ├── PAUSE_7_mp3
+         │      │      ├── PAUSE_1_mp3
+         │      │      ├── PAUSE_2_mp3
+         │      │      ├── PAUSE_4_wav
+         │      │      ├── PAUSE_7_wav
+         │      │      ├── PAUSE_1_wav
+         │      │      ├── PAUSE_2_wav
+         │      │      ├── MELODY_START_mp3
+         │      │      ├── MELODY_END_mp3
+         │      │      ├── MELODY_START_wav
+         │      │      ├── MELODY_END_wav
+         │      │      └── TEST_wav
+         │      └── input_sounds_effects/
+         ├── outputs/
+         │      ├── output_mp3/
+         │      └── output_multispeakers/
+         └── temp_folder/       
+                  └── logs/
+                           ├── config_manager.log
+                           ├── accent_editor.log 
+                           ├── voice_tags_editor.log    
+                           ├── sound_effects_editor.log
+                           └── multispeaker_tts.log
+
+                           
