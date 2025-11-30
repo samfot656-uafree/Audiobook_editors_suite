20aa99ae3a7c8e92cf199aab46a7db479f161193
samfot656-uafree <samfot656@gmail.com>
2025-11-30 16:40:39 +0200

    Add files via upload

---
+++ a0_sb2_book_editors_suite/book_editors_suite/ui/__init__.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+UI компоненти для редакторів книг.
+"""# -*- coding: utf-8 -*-
+"""
+/storage/emulated/0/a0_sb2_book_editors_suite/book_editors_suite/ui/__init__.py
+
+Пакет UI компонентів.
+"""
+
+# Цей файл може бути порожнім або містити імпорти
+
+
+
+
+# -*- coding: utf-8 -*-
+#"""
+#/storage/emulated/0/a0_sb2_book_editors_suite/book_editors_suite/ui/__init__.py
+
+#Пакет UI компонентів.
+
+
+
+
+
+# -*- coding: utf-8 -*-
+#"""
+#/storage/emulated/0/a0_sb2_book_editors_suite/book_editors_suite/ui/__init__.py
+
+#Пакет UI компонентів.
+
+
+
+# book_editors_suite/ui/__init__.py
+#"""
+#UI module - компоненти користувацького інтерфейсу
+#"""
+
+#from .themes import ThemeManager, DarkTheme, LightTheme
+
+#__all__ = [
+#    'ThemeManager',
+#    'DarkTheme', 
+#    'LightTheme'
+#]
+
+# book_editors_suite/ui/__init__.py
+#"""
+#UI module - компоненти користувацького інтерфейсу
+#"""
+
+#from .themes import ThemeManager, DarkTheme, LightTheme
+
+#__all__ = [
+#    'ThemeManager',
+#    'DarkTheme', 
+#    'LightTheme'
+#]
+
+#"""
+#UI module - компоненти користувацького інтерфейсу
+#"""
+
+#from .themes import ThemeManager, DarkTheme, LightTheme
+
+#__all__ = [
+#    'ThemeManager',
+#    'DarkTheme', 
+#    'LightTheme'
+#]
