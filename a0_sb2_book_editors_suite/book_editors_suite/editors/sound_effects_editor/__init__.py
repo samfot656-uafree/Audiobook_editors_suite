70805deffe1c0f5b8054cc3783b85ad679967bc8
samfot656-uafree <samfot656@gmail.com>
2025-11-30 17:02:38 +0200

    Create __init__.py
    Create project folder structure

---
+++ a0_sb2_book_editors_suite/book_editors_suite/editors/sound_effects_editor/__init__.py
@@ -0,0 +1,25 @@
+# book_editors_suite/editors/sound_effects_editor/__init__.py
+"""
+Sound Effects Editor - редактор звукових ефектів
+"""
+
+from .main import SoundEffectsEditor
+
+__all__ = ['SoundEffectsEditor']
+
+# book_editors_suite/editors/sound_effects_editor/__init__.py
+#"""
+#Sound Effects Editor - редактор звукових ефектів
+#"""
+
+#from .main import SoundEffectsEditor
+
+#__all__ = ['SoundEffectsEditor']
+
+#"""
+#Sound Effects Editor - редактор звукових ефектів
+#"""
+
+#from .main import SoundEffectsEditor
+
+#__all__ = ['SoundEffectsEditor']
