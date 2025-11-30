7a07fe2dbfacfb76ffa11d2a8e5670dc34feea9a
samfot656-uafree <samfot656@gmail.com>
2025-11-30 16:59:25 +0200

    Create __init__.py
    Create project folder structure

---
+++ a0_sb2_book_editors_suite/book_editors_suite/editors/voice_tags_editor/__init__.py
@@ -0,0 +1,26 @@
+# book_editors_suite/editors/voice_tags_editor/__init__.py
+"""
+Voice Tags Editor - редактор тегів голосів та пауз
+"""
+
+from .main import VoiceTagsEditor
+
+__all__ = ['VoiceTagsEditor']
+
+
+# book_editors_suite/editors/voice_tags_editor/__init__.py
+#"""
+#Voice Tags Editor - редактор тегів голосів та пауз
+#"""
+
+#from .main import VoiceTagsEditor
+
+#__all__ = ['VoiceTagsEditor']
+
+#"""
+#Voice Tags Editor - редактор тегів голосів та пауз
+#"""
+
+#from .main import VoiceTagsEditor
+
+#__all__ = ['VoiceTagsEditor']
